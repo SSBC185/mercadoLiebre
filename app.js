b0e076a755522727a07bb79c9f7750fa7035e24e
SSBC185 <sam.barrioscabrera@gmail.com>
2023-12-20 15:16:44 -0300

    modifique las rutas

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html"));
+    res.sendFile(path.join(__dirname, "./views/home.html"));
   });
 
-  app.get("/home", (req, res) => {
-    res.redirect('/');
-  });
 
   app.listen(port, () => console.log(`http://localhost:${port}`));

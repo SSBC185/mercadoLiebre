643a2f456129ffbb1530ecceb3775f985a4838db
SSBC185 <sam.barrioscabrera@gmail.com>
2024-02-05 19:16:09 -0300

    formulario

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
   });
 
+  app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+  });
+
 
   app.listen(port, () => console.log(`http://localhost:${port}`));
 
